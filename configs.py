20f72064c130a1deb5f9ad0f81b074a37a25beaa
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-17 17:50:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,11 +14,11 @@ logging.basicConfig(
 class Config(object):
     API_ID = 3845818
     API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
-    BOT_TOKEN = "5426835246:AAGpWNE1EQF5_t56BmcdD8QWSJhCtynh3gk"
+    BOT_TOKEN = "5991767808:AAHKoINN5FcCzmfDsLGp7CKzrl6M5n_FDB4"
     DOWNLOAD_DIR = "./downloads"
     LOGGER = logging
     OWNER_ID = 1443454117
     PRO_USERS = 1443454117
     MONGODB_URI = "mongodb+srv://720p:hevc@cluster0.fw3dwgb.mongodb.net/?retryWrites=true&w=majority"
-    LOG_CHANNEL = int("-1001730306649")
+    LOG_CHANNEL = int("-1001870386819")
     BROADCAST_AS_COPY = bool("False")
